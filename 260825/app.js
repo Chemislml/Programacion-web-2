c939ff20e121b6b376844ff35e643dc39668266d
Chemislml <chavinchema@hotmail.com>
2025-08-31 17:09:30 -0600

    Clase del 26/08/25

---
+++ 260825/app.js
@@ -0,0 +1,11 @@
+variable1 = parseInt(prompt("Ingresa el numero 1"));
+variable2 = parseInt(prompt("Ingresa el numero 2"));
+sumvar = parseInt(variable1 + variable2);
+resvar = parseInt(variable1 - variable2);
+multvar = parseInt(variable1 * variable2);
+divvar = parseInt(variable1 / variable2);
+
+console.log(resutado);
+console.log
+
+//Tarea subir este programa
